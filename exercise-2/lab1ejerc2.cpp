3c24b628635ae91e8bbb7ff2ab17c0ad4c32a9da
kumiko022 <zS25018131@estudiantes.uv.mx>
2025-10-10 19:55:36 -0600

    Se subió el ejercicio2

--- exercise-2/lab1ejerc2.cpp
+++ exercise-2/lab1ejerc2.cpp
@@ -5,8 +5,8 @@ using namespace std;
 int main() {
   int numero;
   float total;
-  cout << "Hoy es un gran día!" cout << endl
-       << "Comencemos escribiendo un número:" << endl;
+  cout << "Hoy es un gran día!" << endl;
+    cout << "Comencemos escribiendo un número:" << endl;
   cin >> numero;
   total = numero * 2;
   cout << total << " es el doble del número que escribiste" << endl;
